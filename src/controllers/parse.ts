11fd724dc83b2781e28c37042712573ea895673c
Samuel Ikoli <ayibanimiikoli@gmail.com>
2024-06-01 16:29:17 +0100

    cleaned up a few things

--- src/controllers/parse.ts
+++ src/controllers/parse.ts
@@ -1,41 +1,60 @@
-import express, { RequestHandler, Request, Response } from 'express'
-import OpenAI from 'openai'
+import express, { RequestHandler, Request, Response } from "express";
+import OpenAI from "openai";
 import {
-    accessBankRegex,
-    fidelityBankRegex,
-    gtBankRegex,
-    ubaBankRegex,
-} from '../parsers';
+  accessBankRegex,
+  fidelityBankRegex,
+  gtBankRegex,
+  ubaBankRegex,
+} from "../parsers";
 
-import { stripCommas, parseSMS } from '../utils/helpers';
+import { stripCommas, parseSMS } from "../utils/helpers";
 
 // Map of the banks we are working with and their corresponding regex, when this scales and more banks are added, we just add the new banks here and their corresponding regex pattern
 const regexMap: { [key: string]: RegExp } = {
-    AccessBank: accessBankRegex,
-    FidelityBank: fidelityBankRegex,
-    GTB: gtBankRegex,
-    UBA: ubaBankRegex,
+  AccessBank: accessBankRegex,
+  FidelityBank: fidelityBankRegex,
+  GTB: gtBankRegex,
+  UBA: ubaBankRegex,
 };
 
-
-
-
 // MAIN CONTROLLER
-export const parseMessage: RequestHandler = async (req: Request, res: Response) => {
-    const { parser, message, senderID } = req.body;
+export const parseMessage: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { parser, message, senderID } = req.body;
+  if (!message || typeof message !== "string") {
+    return res.status(400).json({
+      error:
+        "You did not send in a message or the message is not of type string",
+    });
+  }
 
-    const newMessage = stripCommas(message)
+  if (!senderID || typeof senderID !== "string") {
+    return res.status(400).json({
+      error:
+        "You did not send in a senderID or the senderID is not of type string",
+    });
+  }
 
-    const regex: any = regexMap[senderID];
-    if (!regex) {
-        return res.status(400).json({ error: 'Unsupported sender ID, supported sender Ids are AccessBank, FidelityBank, GTB and UBA' });
-    }
+  const newMessage = stripCommas(message);
 
-    try {
-        const parsedData = parseSMS(regex, newMessage);
-        parsedData.institution = senderID.length > 4 ? senderID.replace(/([A-Z])/g, ' $1').trim() : senderID        
-        res.status(200).json(parsedData);
-    } catch (error: any) {
-        res.status(400).json({ error: error.message });
-    }
-}
+  const regex: any = regexMap[senderID];
+  if (!regex) {
+    return res.status(400).json({
+      error:
+        "Unsupported sender ID, supported sender Ids are AccessBank, FidelityBank, GTB and UBA",
+    });
+  }
+
+  try {
+    const parsedData = parseSMS(regex, newMessage);
+    parsedData.institution =
+      senderID.length > 4
+        ? senderID.replace(/([A-Z])/g, " $1").trim()
+        : senderID;
+    res.status(200).json(parsedData);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
